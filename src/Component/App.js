2567af22f86236ab9dc137ec7d2923423a00abd7
bouzuya <m@bouzuya.net>
2018-12-16 15:20:14 +0900

    add dummy html

---
+++ src/Component/App.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var leaflet = require('react-leaflet');
+
+exports.leafletMap = leaflet.Map;
+exports.leafletMarker = leaflet.Marker;
+exports.leafletTileLayer = leaflet.TileLayer;
--- src/Component/App.purs
+++ src/Component/App.purs
@@ -2,8 +2,12 @@ module Component.App
   ( app
   ) where
 
-import React.Basic (Component, JSX, Self, StateUpdate(..), createComponent, make)
+import React.Basic (Component, JSX, ReactComponent, Self, StateUpdate(..), createComponent, element, make)
 import React.Basic.DOM as H
+
+foreign import leafletMap :: forall props. ReactComponent { | props }
+foreign import leafletMarker :: forall props. ReactComponent { | props }
+foreign import leafletTileLayer :: forall props. ReactComponent { | props }
 
 type Props =
   {}
@@ -37,7 +41,40 @@ render self =
         ]
       }
     , H.div
-      { className: "body" }
+      { className: "body"
+      , children:
+        [ element
+            leafletMap
+            { center: [35, 135]
+            , zoom: 10
+            , children:
+              [ element
+                  leafletTileLayer
+                  { url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  }
+              , element
+                  leafletMarker
+                  { position: [35, 135]
+                  }
+              ]
+            }
+        , H.label_
+          [ H.span_ [ H.text "latitude" ]
+          , H.input
+            { value: "35"
+            }
+          ]
+        , H.label_
+          [ H.span_ [ H.text "longitude" ]
+          , H.input
+            { value: "135"
+            }
+          ]
+        , H.button
+          { children: [ H.text "OK" ]
+          }
+        ]
+      }
     , H.div
       { className: "footer" }
     ]
